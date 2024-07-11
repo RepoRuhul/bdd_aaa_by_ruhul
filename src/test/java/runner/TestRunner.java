2eddaacde2d6b8f1e6f0d14ca950af2adb9186b7
ruhul@Rafas_laptop <mdrhlkdus@gmail.comgit config --global user.email mdrhlkdus@gmail.comgit config --global user.email mdrhlkdus@gmail.com>
2024-07-10 20:39:55 -0400

    day one

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,7 +3,7 @@ package runner;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
+//
 @RunWith(Cucumber.class)
 @CucumberOptions(	tags = "@smoke",
 					features = "src/test/java/features",
